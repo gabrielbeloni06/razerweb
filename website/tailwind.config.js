7df5e161fc6b51fc24cd03f2f554472e7c4a3c55
gabrielbeloni <gabrielbelonibr@gmail.com>
2025-11-25 15:04:57 -0300

    create: structure

---
+++ website/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'razer-green': '#44d62c',
+      }
+    },
+  },
+  plugins: [],
+}
